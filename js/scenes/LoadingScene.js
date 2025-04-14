4487e7feea76fd000242b70bbeffcd97dd2190e2
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-04-14 02:20:22 +0200

    chore: cleanup loading scene

--- js/scenes/LoadingScene.js
+++ js/scenes/LoadingScene.js
@@ -8,28 +8,24 @@ export default class LoadingScene extends Phaser.Scene {
     init(){
         document.querySelector(`.miccheck-wrapper`).style.display = `none`;   
         document.querySelector(`.loading-wrapper`).style.display = `flex`;
+        setTimeout(() => {
+            this.scene.start(`menu-scene`);
+        }, 4000);
     }
 
     preload() {
         this.load.svg('background', '../assets/img/background.svg', { scale: 1 });
-
-        this.load.json(`map`, `../assets/matterJson/mapGround.json`);
-
         this.load.svg('foreground', '../assets/img/foreground.svg', { scale: 1 });
-
         this.load.svg('caveDarkness', '../assets/img/caveDarkness.svg', { scale: 1 });
-
+ 
         this.load.image(`treeObstacle`, `../assets/img/treeObstacle.png`);
-
         this.load.image('platform', '../assets/img/platform.png');
-
         this.load.image('ring', '../assets/img/holyRing.png');
-
-        this.load.atlas(`character`, `../assets/img/character.png`, `../assets/matterJson/character_atlas.json`);
-        
-        this.load.atlas(`end`, `../assets/img/end.png`, `../assets/matterJson/end_atlas.json`);
-
         this.load.image(`pillarObstacle`, `../assets/img/pillarObstacle.png`);
-        this.scene.start(`menu-scene`);
+        
+        this.load.atlas(`character`, `../assets/img/character.png`, `../assets/matterJson/character_atlas.json`);
+        this.load.atlas(`end`, `../assets/img/end.png`, `../assets/matterJson/end_atlas.json`);
+        
+        this.load.json(`map`, `../assets/matterJson/mapGround.json`);
     }
 }
