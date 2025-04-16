2d85d90caae861883d9dc9c1d403449c31265393
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-04-16 03:15:52 +0200

    feat: environment class with some presets

--- js/classes/Environment.js
+++ js/classes/Environment.js
@@ -1,4 +1,31 @@
 
 export default class Environment {
+    static #scene = undefined;
+    static set scene(scene) {
+        this.#scene = scene;
+    };
 
+    // visuals
+    static foregroundZ = 10;
+    static interactZ = 5;
+    static backgroundZ = 0;
+    static #textZ = 15;
+    static #textFont = { fontFamily: `runes`, color: `white`, fontSize: `2rem` };
+
+    // Colllision
+    static #floorLabel = 'floor';
+
+    static get floorLabel() {
+        return this.#floorLabel;
+    }
+
+    static addText(posX, posY, text) {
+        
+        const textObject = this.#scene.add.text(posX, posY, text, this.#textFont);
+        textObject.setOrigin(0.5, 0.5);
+        textObject.w = this.#textZ;
+        textObject.z = this.#textZ;
+        textObject.depth = this.#textZ;
+        return textObject;
+    }
 }
