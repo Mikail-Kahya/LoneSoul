6c0745bcba9f9302cac169976d79e73f58d937bc
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2025-04-19 19:28:07 +0200

    fix: removed the debugging

--- js/classes/Debugger.js
+++ js/classes/Debugger.js
@@ -2,16 +2,16 @@
 // Allows for logging and deactivating it on release builds
 
 export default class Debugger {
-    static #log = true;
-    static #draw = true;
-    static #playerPos = true;
+    static #log = false;
+    static #draw = false;
+    static #playerPos = false;
 
     static get shouldDraw() {
         return this.#draw;
     }
 
     static get playerPos() {
-        return this.#playerPos ? {x: 8000, y: 0 } : {x: 0, y: 0};
+        return this.#playerPos ? {x: 2000, y: 0 } : {x: 0, y: 0};
     }
     
     static log(object) {
